f5f36bdebaf4f2097e57f9fa7ea2b7ad4c8d8cea
Kathy * <Kathy@KathyLambert.me>
2019-06-09 22:29:52 -0400

    Started ex1.js.

--- JavaScript/ex1.js
+++ JavaScript/ex1.js
@@ -2,31 +2,78 @@
 // Javascript Console in Google Chrome. Once you have an answer  to the questions then
 // check them by copying them and running it in the console yourself line by line
 
-//Evaluate the below:
-5 + '34'
-5 - '4'
-10 % 5
-5 % 10
-'Java' + 'Script'
-' ' + ' '
-' ' + 0
-true + true
-true + false
-false + true
-false - true
-3 - 4
-'Bob' - 'bill'
+// // Evaluate the below:
+// 5 + '34' // '534'
+// console.log(5 + '34')
 
-//Evaluate the below comparisons:
-5 >= 1
-0 === 1
-4 <= 1
-1 != 1
-'A' > 'B'
-'B' < 'C'
-'a' > 'A'
-'b' < 'A'
-true === false
-true != true
+// 5 - '4' // ?
+// console.log(5 - '4') // 1
 
-// Make the string: "Hi There! It's "sunny" out" by using the + sign:
+// 10 % 5 // 0
+// console.log(10 % 5 )
+
+// 5 % 10 //5
+// console.log(5 % 10)
+
+// 'Java' + 'Script' //'JavaScript
+// console.log('Java' + 'Script')
+
+// ' ' + ' ' // '  '
+// console.log(' ' + ' ')
+
+// ' ' + 0 // ' 0'
+// console.log(' ' + 0)
+
+// true === 1 && false === 0
+// true + true // 2
+// console.log(true + true)
+
+// // true + false // 1
+// console.log(true + false)
+
+// // false + true // 1
+// console.log(false + true)
+
+// // false - true // -1
+// console.log(false - true)
+
+// // 3 - 4 // -1
+// console.log(3 - 4)
+
+// // 'Bob' - 'bill' // NaN
+// console.log('Bob' - 'bill')
+
+// // Evaluate the below comparisons as true or false:
+
+// 5 >= 1 //
+
+
+// 0 === 1 //
+
+
+// 4 <= 1 //
+
+
+// 1 !== 1 //
+
+
+// 'A' > 'B' //
+
+
+// 'B' < 'C' //
+
+
+// 'a' > 'A' //
+
+
+// 'b' < 'A' //
+
+
+// true === false //
+
+
+// true !== true //
+
+
+
+// // Make the string: "Hi There! It's "sunny" out" by using the + sign:
